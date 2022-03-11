fde425f1038f200505eb99a24b0cc3542452ad80
tonywkx <tonywk@ya.ru>
2022-03-11 23:30:03 +0400

    feat: add drag and drop

--- app.js
+++ app.js
@@ -24,13 +24,41 @@ async function getTasks() {
 getTasks();
 
 
-const newText = document.querySelector('p');
+const newText = document.querySelector('.empty');
 
 function showU(data1) {
     newText.textContent = data1[2].text;
 }
 
+function onDragStart(event) {
+    event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+  
+    event
+      .currentTarget
+      .style
+      .backgroundColor = 'yellow';
+  }
 
+
+function onDragOver(event) {
+    event.preventDefault();
+  }
+ 
+  
+function onDrop(event) {
+    const id = event
+      .dataTransfer
+      .getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+    event
+    .dataTransfer
+    .clearData();
+}
+  
 //
 
 let nav = 0;
